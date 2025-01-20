cc4d877a6276eb1222fa7b0858190e6bd86eaab4
rita1204 <pittixichan@gmail.com>
2025-01-20 12:52:13 +0900

    update sources path

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TransitionableTab",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources/TransitionableTab"
+        ),
         .testTarget(
             name: "TransitionableTabTests",
             dependencies: ["TransitionableTab"]),
